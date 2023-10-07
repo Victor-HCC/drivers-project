5252c3c582c605c74b3916b0bd818c74ac30aced
Victor <vicktorcori@hotmail.com>
2023-10-07 11:40:50 -0300

    Modelos creados para la db

--- server/src/models/Driver.js
+++ server/src/models/Driver.js
@@ -4,9 +4,41 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('Driver', {
-    name: {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    forname: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    surname: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    image: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    nationality: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    dob: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    created: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    }
+  },
+  {
+    timestamps: false
   });
 };
